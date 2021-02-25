e043fafa86104b87479b40f3009cca7f31d425c8
ulvi macid <ulvizm@code.edu.az>
2021-02-25 15:17:08 +0400

    adding MediatR pattern

---
+++ Reactivities/API/Controllers/ActivitiesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Activities;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ActivitiesController : ControllerBase
+    {
+        private IMediator _mediatr;
+        public ActivitiesController(IMediator mediatr)
+        {
+            _mediatr = mediatr;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> List()
+        {
+            return await _mediatr.Send(new List.Query());
+
+        }
+        
+    }
+}
