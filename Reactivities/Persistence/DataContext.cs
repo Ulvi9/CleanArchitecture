cb8d81efef621bed71a62f53060f7503cd61c247
ulvi macid <ulvizm@code.edu.az>
2021-02-25 12:31:45 +0400

    create Projects

---
+++ Reactivities/Persistence/DataContext.cs
@@ -0,0 +1,28 @@
+﻿
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace Persistence
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+        public DbSet<Domain.Activity> Activities { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           // base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Value>().HasData(
+                new Value {Id = 1, Name = "Value1"},
+                new Value {Id = 2, Name = "Value2"},
+                new Value {Id = 3, Name = "Value3"}
+            );
+        }
+    }
+}
